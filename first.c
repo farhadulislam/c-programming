983a00230d9979fbafa060fe53849043ae2722dd
Farhad Ul Islam <farhad.islam@outlook.com>
2018-03-31 17:45:54 +0100

    first program
    for loop added
    
    first program : for loop removed

---
+++ first.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ int main(){
+
+
+ void addNumber();
+   printf("This is a C program\n");
+
+   int number;
+   printf("Enter an integer");
+
+   scanf("%d", &number);
+   printf("you've entered %d", number);
+
+  return 0;
+
+}
