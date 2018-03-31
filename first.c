4e2c25dc2551e46f327615a41ea243ee0a5c190d
Farhad Ul Islam <farhad.islam@outlook.com>
2018-03-31 11:37:22 +0100

    first program

---
+++ first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+ int main(){
+
+   printf("This is a C program\n");
+
+   int number;
+   printf("Enter an integer");
+
+   scanf("%d", &number);
+   printf("you've entered %d", number);
+
+
+
+}
