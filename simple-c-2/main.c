ad7d61bbece2cdefcccc3e2865877065666ac1c2
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-02 23:38:21 +0100

    addeda prototype function

--- simple-c-2/main.c
+++ simple-c-2/main.c
@@ -6,12 +6,17 @@
 char s[]="Heal the world";
 
 int main(void) {
-
+void showMe();
 char c;
 int i, j;
-for (i=0,j=strlen(s)-1;i<j;i++,j--) /* strlen(s)-1 ? */
-c=s[i], s[i]=s[j], s[j]=c;
+for (i=0,j=strlen(s)-1;i<j;i++,j--){
+    c=s[i], s[i]=s[j], s[j]=c;
+} /* strlen(s)-1 ? */
+
 
 printf("%s\n",s);
+showMe();
 return 0;
 }
+void showMe(){
+printf("This is it");}
