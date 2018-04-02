2e0d2381f52c056a0a03909a483b3f8ba3937d76
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-02 01:25:46 +0100

    endln added on second.c

--- second.c
+++ second.c
@@ -9,20 +9,20 @@
    printf("This is a C program\n");
 
    int number1;
-   printf("Enter a number to add");
+   printf("Enter a number to add \n");
 
-   scanf("%d", &number1);
-   printf("you've entered %d", number1);
+   scanf("%d \n", &number1);
+   printf("you've entered %d \n", number1);
 
    int number2;
-   printf("Enter another number to add");
+   printf("Enter another number to add \n");
 
-   scanf("%d", &number2);
-   printf("you've entered %d", number2);
+   scanf("%d \n", &number2);
+   printf("you've entered %d \n", number2);
 
    int answer = addNumber(number1, number2);
 
-   printf("Answer is %d", answer);
+   printf("Answer is %d \n", answer);
 
 
 
@@ -30,7 +30,12 @@
   return 0;
 
 }
-int addNumber(int a , int b){
+
+ int addNumber(int a , int b){
 
        return (a+b);
    }
+
+double doAverage (double [] cost){
+  return 0.00;
+}
